f3bf5fe96d94b23d3d97103a86506bf76c3e571b
Moreno <jesusmorenomm1@gmail.com>
2026-01-27 20:32:09 -0600

    API final

--- src/db/conexion.ts
+++ src/db/conexion.ts
@@ -2,14 +2,15 @@ import { DataSource } from "typeorm";
 import { Student } from "../models/studentsModel";
 import { Teacher } from "../models/teachersModel";
 import { Course } from "../models/coursesModel";
+import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "",
-    database: "courses",
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "courses",
     synchronize: false,
     logging: true,
     entities: [Student, Teacher, Course],
